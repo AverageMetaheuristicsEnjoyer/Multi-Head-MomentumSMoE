09e8813558c61bd4fdd8599fba6b6d163b8146db
AverageMetaheuristicsEnjoyer <AverageMetaheuristicsEnjoyer@proton.me>
2025-07-17 23:50:12 +0300

    chore: clear unused parts and rename some staff

--- gates.py
+++ gates.py
@@ -27,55 +27,85 @@ class BaseGate(nn.Module):
         return self.loss is not None
 
 class CustomNaiveGate_Balance_SMoE(BaseGate):
-    def __init__(self, d_model, num_expert, world_size, top_k=2, g_blance=False):
+    def __init__(self, d_model, num_expert, world_size, top_k=2, aux_blance=True):
         super().__init__(num_expert, world_size)
-        self.gate = nn.Linear(d_model, self.tot_expert)
+        self.gate = nn.Linear(d_model, self.tot_expert, bias=False)
         self.top_k = top_k
-        self.dense_moe_flag = False
-        self.g_blance = g_blance
+        self.aux_blance = aux_blance
         self.loss = None
 
-    def set_load_balance(self, gate, gate_top_k_idx):
+    def _calculate_load_balance_loss(self, router_probs, top_k_indices):
+        with torch.no_grad():
+            one_hot_indices = F.one_hot(top_k_indices, self.tot_expert).float()
+            one_hot_indices = torch.sum(one_hot_indices, dim = 2)
+            f_i = one_hot_indices.mean(dim=(0, 1))
 
-        score = F.softmax(gate, dim=-1)
-        valid_idx = gate_top_k_idx[gate_top_k_idx > -1]
-        fraction_expert = (
-            torch.scatter_add(
-                torch.zeros(self.tot_expert, device=valid_idx.device),
-                0,
-                valid_idx,
-                torch.ones_like(valid_idx, dtype=torch.float),
-            )
-            / valid_idx.numel()
-        )
-        prob_expert = score.sum(dim=0) / valid_idx.numel()
+        P_i = torch.mean(router_probs.float(), dim=(0, 1))
 
-        loss = (fraction_expert * prob_expert).sum() * self.tot_expert
+        loss = (f_i * P_i).sum() * self.tot_expert
         self.loss = loss
 
     def forward(self, inp, return_all_scores=False):
+        logits = self.gate(inp)
 
-        gate = self.gate(inp)
+        top_k_logits, top_k_indices = torch.topk(
+            logits, k=self.top_k, dim=-1, largest=True, sorted=False
+        )
 
-        if self.dense_moe_flag:
-            gate = torch.ones_like(gate)  # average the importance of all experts
-            gate_top_k_val, gate_top_k_idx = torch.topk(
-                gate, k=self.tot_expert, dim=-1, largest=True, sorted=False
-            )
-            gate_top_k_val = gate_top_k_val.view(-1, self.tot_expert)
-        else:
-            gate_top_k_val, gate_top_k_idx = torch.topk(
-                gate, k=self.top_k, dim=-1, largest=True, sorted=False
-            )  # [.. x top_k]
-            gate_top_k_val = gate_top_k_val.view(-1, self.top_k)  # (BxL) x 1 x top_k
+        router_probs = F.softmax(top_k_logits, dim=-1)
 
-        gate_score = F.softmax(gate_top_k_val, dim=-1)
-        if self.g_blance:
-            self.set_load_balance(gate, gate_top_k_idx)
+        if self.training and self.aux_blance:
+            self._calculate_load_balance_loss(router_probs, top_k_indices)
 
         if return_all_scores:
-            return gate_top_k_idx, gate_score, gate
-        return gate_top_k_idx, gate_score
+            return top_k_indices, router_probs, logits
+        return top_k_indices, router_probs
+
+# class EF21Gate(BaseGate):
+#     def __init__(self, d_model, num_expert, world_size, top_k=2):
+#         super().__init__(num_expert, world_size)
+#         self.gate = nn.Linear(d_model, self.tot_expert)
+#         self.top_k = top_k
+#         self.top_kc = 1
+#         self.loss = None
+
+#         self.register_buffer('g_t', None)
+
+#     def forward(self, inp, g_t):
+#         gate = self.gate(inp)
+
+#         if self.g_t is None or self.g_t.shape != gate.shape:
+#             initial_top_k_val, initial_top_k_idx = torch.topk(
+#                 gate, k=self.top_k, dim=-1, largest=True, sorted=False
+#             )
+#             self.g_t = torch.zeros_like(gate)
+#             self.g_t.scatter_(dim=-1, index=initial_top_k_idx, src=initial_top_k_val)
+
+
+#         gate = gate - self.g_t.detach()
+
+#         gate_top_k_val, gate_top_k_idx = torch.topk(
+#             gate, k=self.top_k, dim=-1, largest=True, sorted=False
+#         )
+
+#         c_t = torch.zeros_like(gate)
+#         c_t = c_t.scatter_(
+#             dim = -1,
+#             index = gate_top_k_idx,
+#             src = gate_top_k_val,
+#         )
+#         new_g_t = self.g_t + c_t
+
+#         gate_top_k_val, gate_top_k_idx = torch.topk(
+#             new_g_t, k=self.top_k, dim=-1, largest=True, sorted=False
+#         )
+
+#         gate_score = F.softmax(gate_top_k_val.view(-1, self.top_k), dim=-1)
+
+#         self.g_t = new_g_t.detach()
+
+#         return gate_top_k_idx, gate_score, new_g_t
+
 
 def _one_hot_with_dtype(data, num_classes, dtype, hot_value=1):
     result = torch.zeros([data.size(0), num_classes], device=data.device, dtype=dtype)
